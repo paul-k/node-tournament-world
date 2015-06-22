b3ef5470b929bba790a5a048c632ae336e7c7567
Paul Kennedy <paulk85@outlook.com>
2015-06-22 23:22:01 +0100

    missed server.js on last commit

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
 
     'use strict';
 
-    var express = require('./config/express'),
-        configureRoutes = require('./config/routes');
+    var express = require('./app/config/express'),
+        configureRoutes = require('./app/config/routes');
 
     configureRoutes(express.app);
 
