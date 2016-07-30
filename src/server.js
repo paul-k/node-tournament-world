dadc1f61a74c1e03f88cffe3a9c0ca85b9013a34
Paul Kennedy <paulk85@outlook.com>
2016-07-30 16:54:05 +0100

    removing gulpfile from dist as now using pm2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,3 @@
-// moved required physical files
-// -- -- -- -- -- -- -- -- -- -- -- -- --
-import 'file?name=gulpfile.js!gulpfile.js';
-// -- -- -- -- -- -- -- -- -- -- -- -- --
-
-
 import express from './config/express';
 import configureRoutes from './config/routes';
 import nodeJsx from 'node-jsx';
