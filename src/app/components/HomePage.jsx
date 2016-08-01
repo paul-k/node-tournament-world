8a12bb594bb55f7276a4b2e03a09ef3fa17bda23
Paul Kennedy <paulk85@outlook.com>
2016-08-01 16:59:31 +0100

    4 names, 3 rounds

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const HomePage = () => (
-	<h1>Hello World from React Home Page</h1>
-);
+class HomePage extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>participants</h2>
+				<ul>
+					<li>Dave</li>
+					<li>Alvin</li>
+					<li>Simon</li>
+					<li>Theodore</li>
+				</ul>
+
+				<h2>round 1</h2>
+				<ul>
+					<li>Dave - Alvin</li>
+					<li>Simon - Theodore</li>
+				</ul>
+
+				<h2>round 2</h2>
+				<ul>
+					<li>Dave - Simon</li>
+					<li>Alvin - Theodore</li>
+				</ul>
+
+				<h2>round 3</h2>
+				<ul>
+					<li>Dave - Theodore</li>
+					<li>Alvin - Simon</li>
+				</ul>
+			</div>
+		);
+	}
+}
 
 export default HomePage;
