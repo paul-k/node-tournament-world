e93c7886257fc3bd2e2d45ce04cde28aa599523d
Paul Kennedy <paulk85@outlook.com>
2016-08-06 22:39:31 +0100

    temporary comments on home controller for eslint

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDomServer from 'react-dom/server';
+//import React from 'react';
+//import ReactDomServer from 'react-dom/server';
 
-import App from 'app/components/App';
-let ReactApp = React.createFactory(App);
+//import App from 'app/components/App';
+//let ReactApp = React.createFactory(App);
 
 var controller = {
 	index: function(req, res) {
