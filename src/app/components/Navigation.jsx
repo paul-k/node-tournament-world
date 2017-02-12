1395953c3d855c9d166d24c09729c5156ce96287
Paul Kennedy <paulk85@outlook.com>
2017-02-12 12:55:25 +0000

    removed the account api controller

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -7,13 +7,6 @@ import { Link } from 'react-router';
 
 const Navigation = () => (
 	<nav>
-		<b>api</b> :
-		<ul>
-			<li><a href="/api/account" target="_blank">Account</a></li>
-			<li><a href="/api/account/signin" target="_blank">Sign in</a></li>
-			<li><a href="/api/account/register" target="_blank">Register</a></li>
-		</ul>
-		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<b>app</b> :
 		<ul>
 			<li><Link to="/" activeClassName="active">home</Link></li>
