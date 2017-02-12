c7afcc45c069ff2c5a6811fd26b474815d3ef99d
Paul Kennedy <paulk85@outlook.com>
2017-02-12 11:42:46 +0000

    renamed HomePage to TournamentPage

--- src/app/components/RoutesConfig.jsx
+++ src/app/components/RoutesConfig.jsx
@@ -3,13 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import Container from 'app/components/Container';
 
-import HomePage from 'app/components/HomePage';
+import TournamentPage from 'app/components/TournamentPage';
 import TestPage from 'app/components/TestPage';
 import NotFound from 'app/components/NotFound';
 
 const RoutesConfig = (
 	<Route path="/" component={ Container }>
-		<IndexRoute component={ HomePage } />
+		<IndexRoute component={ TournamentPage } />
 		<Route path="/test" component={ TestPage }></Route>
 		<Route path="*" component={ NotFound }></Route>
 	</Route>
